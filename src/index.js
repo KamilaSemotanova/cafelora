294039892ddd2115d313a37747d05adc8ce73152
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-04-29 17:02:50 +0200

    prepare for routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './style.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Banner } from './pages/HomePage/components/Banner';
+import { Menu } from './pages/HomePage/components/Menu';
+import { Galerie } from './pages/HomePage/components/Galerie';
+import { Contact } from './pages/HomePage/components/Contact';
 import { HomePage } from './pages/HomePage/index.js';
 
 const appElement = document.querySelector('#app');
 
-appElement.append(HomePage())
+appElement.append(
+  HomePage(),
+  Header(),
+  Banner(),
+  Menu({ drinks: 'loading' }),
+  Galerie(),
+  Contact(),
+  Footer(),
+);
+
+const { pathname } = window.location;
+
+if (pathname === '/') {
+  document.querySelector('#app').append(HomePage());
+}
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,15 +1,6 @@
-import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
-import { Banner } from './Banner';
-import { Menu } from './Menu';
-import { Galerie } from '../Galerie';
-import { Contact } from '../Contact';
-
 export const HomePage = () => {
   const element = document.createElement('main');
   element.setAttribute('id', 'main');
-
-  element.append(Header(), Banner(), Menu({ drinks: 'loading' }),Galerie(), Contact(),Footer());
 
   return element;
 };
