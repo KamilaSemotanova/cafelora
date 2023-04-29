790121304a5f81ebf97976e4ca254314534975a9
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-04-29 13:36:17 +0200

    add drink as component

--- src/pages/HomePage/Menu/DrinkList/index.js
+++ src/pages/HomePage/Menu/DrinkList/index.js
@@ -1,73 +1,10 @@
+import { Drink } from '../components/Drink';
 import './style.css';
 
 export const DrinkList = () => {
   const element = document.createElement('div');
   element.classList.add('drinks-list');
 
-  element.innerHTML += `
-  <div class="drink">
-  <div class="drink__product">
-    <div class="drink__cup">
-      <img src="https://cafelora.kodim.app/assets/cups/espresso.png">
-    </div>
-    <div class="drink__info">
-      <h3>Espresso</h3>
-      <div class="layer">
-        <div class="layer__color" style="background-color: #613916"></div>
-        <div class="layer__label">espresso</div>
-      </div>
-    </div>
-  </div>
-  <div class="drink__controls">
-    <button class="order-btn">
-      Objednat
-    </button>
-  </div>
-</div>
-
-<div class="drink">
-  <div class="drink__product">
-    <div class="drink__cup">
-      <img src="https://cafelora.kodim.app/assets/cups/doppio.png">
-    </div>
-    <div class="drink__info">
-      <h3>Doppio</h3>
-      <div class="layer">
-        <div class="layer__color" style="background-color: #613916"></div>
-        <div class="layer__label">espresso</div>
-      </div>
-    </div>
-  </div>
-  <div class="drink__controls">
-    <button class="order-btn">
-      Objednat
-    </button>
-  </div>
-</div>
-
-<div class="drink">
-  <div class="drink__product">
-    <div class="drink__cup">
-      <img src="https://cafelora.kodim.app/assets/cups/lungo.png">
-    </div>
-    <div class="drink__info">
-      <h3>Lungo</h3>
-      <div class="layer">
-        <div class="layer__color" style="background-color: #b0dee1"></div>
-        <div class="layer__label">voda</div>
-      </div>
-      <div class="layer">
-        <div class="layer__color" style="background-color: #613916"></div>
-        <div class="layer__label">espresso</div>
-      </div>
-    </div>
-  </div>
-  <div class="drink__controls">
-    <button class="order-btn">
-      Objednat
-    </button>
-  </div>
-  </div>
-`;
+  element.append(Drink());
   return element;
 };
